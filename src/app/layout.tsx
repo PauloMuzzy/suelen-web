4b59528487b9009dce2dd3cd533fd3d8c9348f89
PauloMuzzy <paulokriger@gmail.com>
2024-09-08 15:59:11 -0300

    chore: ajustando estilos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
+import { Roboto } from '@next/font/google';
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import StyledComponentsRegistry from "../../lib/registry";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={roboto.className}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
